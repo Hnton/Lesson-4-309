e462a748cd5661265db9633988811cb03ca5ae81
Mikael Hinton <43019867+Hnton@users.noreply.github.com>
2020-04-08 03:54:33 -0400

    Buffer added and Replace method kinda added

--- memoryPage/src/main.rs
+++ memoryPage/src/main.rs
@@ -3,13 +3,55 @@ use rand::Rng;
 
 
 fn main() {
+
+    // Buffer size (Manually change for now to get 3, 5, 10)
+    let buffer_size = 3;
+
+    // Random number generator
     let mut random_number = rand::thread_rng();
+
+    // Creating Vec of 100 numbers
     let mut numbers = Vec::<i64>::with_capacity(100);
     
+    // Pushing 100 random numbers from 0 - 20 into Vec
     for _ in 0..100 {
         numbers.push(random_number.gen_range(0,20));
     }
-    println!("Random Number: {:#?}", numbers);
-    println!("Size: {}", numbers.len());
+
+    // Cloning numbers
+    let mut numbers_clone = numbers.clone();
+
+    // Creating buffer
+    let mut buffer = Vec::<i64>::with_capacity(buffer_size);
+
+    // Prints original 100 numbers
+    println!("Original: {:#?}", numbers);
+
+    // Pushes buffer_size from 100 numbers into buffer
+    for i in 0..buffer_size {
+        buffer.push(numbers_clone[i]);
+    }
+
+    // Creates buffer clone
+    let mut buffer_clone = buffer.clone();
+
+    // Removes buffer_size from 100 numbers
+    for n in 0..buffer_size {
+        numbers_clone.remove(0);
+    }
+
+    // Prints buffer at start
+    println!("buffer {:#?}", buffer_clone);
+
+    // REPLACE IN BUFFER
+
+    // Removes index 0 from buffer 
+    buffer_clone.remove(0);
+    // Inserts index 0 from 100 numbers into index 0 from buffer
+    buffer_clone.insert(0, numbers_clone[0]);
+    // Removes index 0 from 100 numbers
+    numbers_clone.remove(0);
+
+    println!("Changed buffer: {:#?}", buffer_clone);
 
 }
--- memoryPage/target/debug/build/winapi-cb47de7e3d955011/build_script_build-cb47de7e3d955011.d
+++ memoryPage/target/debug/build/winapi-cb47de7e3d955011/build_script_build-cb47de7e3d955011.d
@@ -1,5 +1,5 @@
-D:\GIT\projects\Lesson-4-309\memoryPage\target\debug\build\winapi-cb47de7e3d955011\build_script_build-cb47de7e3d955011.exe: C:\Users\Ethan\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-0.3.8\build.rs
+C:\Users\Mikae\OneDrive\Documents\GitHub\Lesson-4-309\memoryPage\target\debug\build\winapi-cb47de7e3d955011\build_script_build-cb47de7e3d955011.exe: C:\Users\Mikae\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-0.3.8\build.rs
 
-D:\GIT\projects\Lesson-4-309\memoryPage\target\debug\build\winapi-cb47de7e3d955011\build_script_build-cb47de7e3d955011.d: C:\Users\Ethan\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-0.3.8\build.rs
+C:\Users\Mikae\OneDrive\Documents\GitHub\Lesson-4-309\memoryPage\target\debug\build\winapi-cb47de7e3d955011\build_script_build-cb47de7e3d955011.d: C:\Users\Mikae\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-0.3.8\build.rs
 
-C:\Users\Ethan\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-0.3.8\build.rs:
+C:\Users\Mikae\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-0.3.8\build.rs:
