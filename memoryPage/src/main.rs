84ffdaeb00a180f215784ed5ae931b35944f10df
EthanRhodes <31512143+EthanRhodes@users.noreply.github.com>
2020-04-12 04:02:56 -0700

    made report struct

--- memoryPage/src/main.rs
+++ memoryPage/src/main.rs
@@ -1,6 +1,6 @@
 extern crate rand;
 use rand::Rng;
-mod algorithm;
+mod fifo();
 
 #[derive(Debug)]
 pub struct Page {
@@ -19,6 +19,32 @@ impl Page {
         self.reference = true;
     }
 }
+
+pub struct Report {
+    pub hits: i64,
+    pub removes: i64
+    pub faults: i64
+}
+
+impl Report{
+    pub fn new() -> Report
+    Report{
+        hits: 0,
+        removes: 0,
+        faults: 0
+    }
+    fn update_hits(&mut self, hit: &i64 ){
+        self.hits = hit; 
+    }
+    fn update_removes(&mut self, remove: &i64 ){
+        self.removes = remove; 
+    }
+    fn update_faults(&mut self, fault: &i64 ){
+        self.faults = fault; 
+    }
+}
+
+
 fn main() {
 
     // Buffer size (Manually change for now to get 3, 5, 10)
@@ -28,21 +54,21 @@ fn main() {
     let mut random_number = rand::thread_rng();
 
     // Creating Vec of 100 numbers
-    let mut numbers = Vec::<i64>::with_capacity(100);
+    let mut ref_string = Vec::<i64>::with_capacity(100);
     
     // Pushing 100 random numbers from 0 - 19 into Vec
     for _ in 0..100 {
-        numbers.push(random_number.gen_range(0,20));
+        ref_string.push(random_number.gen_range(0,20));
     }
 
     // Cloning numbers
-    let mut numbers_clone = numbers.clone();
+    let mut numbers_clone = ref_string.clone();
 
     // Creating buffer
     let mut buffer = Vec::<i64>::with_capacity(buffer_size);
 
     // Prints original 100 numbers
-    println!("Original: {:#?}", numbers);
+    println!("Original: {:#?}", ref_string);
 
     // Pushes buffer_size from 100 numbers into buffer
     for i in 0..buffer_size {
@@ -71,15 +97,38 @@ fn main() {
 
     println!("Changed buffer: {:#?}", buffer_clone);
 
-    let mut p = Page::new(numbers_clone[0]);
-    print!("{:#?}", p);
-    p.update_ref();
-    print!("{:#?}", p);
+    // let mut p = Page::new(numbers_clone[0]);
+    // print!("{:#?}", p);
+    // p.update_ref();
+    // print!("{:#?}", p);
 
 
 }
 
-// pub fn fifo()
-// {
+pub fn fifo(num: i64)
+{
+    let buffer_size = 3;
+    let mut frame_buffer = Vec::<i64>::with_capacity(buffer_size);
 
-// }
+
+}
+
+pub fn nru()
+{
+
+}
+
+pub fn lru()
+{
+
+}
+
+pub fn second_chance()
+{
+
+}
+
+pub fn clock()
+{
+
+}
--- memoryPage/target/rls/debug/deps/memoryPage-a55f432a6090f74e.d
+++ memoryPage/target/rls/debug/deps/memoryPage-a55f432a6090f74e.d
@@ -1,6 +1,5 @@
-d:\GIT\projects\Lesson-4-309\memoryPage\target\rls\debug\deps\memoryPage-a55f432a6090f74e.rmeta: src\main.rs src\algorithm.rs
+d:\GIT\projects\Lesson-4-309\memoryPage\target\rls\debug\deps\memoryPage-a55f432a6090f74e.rmeta: src\main.rs
 
-d:\GIT\projects\Lesson-4-309\memoryPage\target\rls\debug\deps\memoryPage-a55f432a6090f74e.d: src\main.rs src\algorithm.rs
+d:\GIT\projects\Lesson-4-309\memoryPage\target\rls\debug\deps\memoryPage-a55f432a6090f74e.d: src\main.rs
 
 src\main.rs:
-src\algorithm.rs:
